ba456fe4d1501d26571c1be174cf47d85f2a23d6
Dzmitry Arhei <demetris167@gmail.com>
2023-04-28 22:31:03 +0300

    lesson 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import Card from './components/Card';
+import Drawer from './components/Drawer';
+import Header from './components/Header';
+
 function App() {
-  return <div className="wrapper"></div>;
+  return (
+    <div className="wrapper clear">
+      <Drawer />
+      <Header />
+      <div className="content p-40">
+        <div className="d-flex align-center justify-between mb-40">
+          <h1>Все кроссовки</h1>
+          <div className="search-block">
+            <img src="/img/search.svg" alt="search" />
+            <input type="text" placeholder="Поиск..." />
+          </div>
+        </div>
+        <div className="d-flex">
+          <Card />;
+          <Card />;
+          <Card />;
+          <Card />;
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
