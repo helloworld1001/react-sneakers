702b6629bffc300f3e266d9b63201ed2824f8c50
Dzmitry Arhei <demetris167@gmail.com>
2023-05-29 18:24:13 +0300

    bugfix 1

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ function Header(props) {
 
   return (
     <header className="d-flex justify-between align-center p-40">
-      <Link to="/">
+      <Link to="">
         <div className="d-flex align-center">
           <img width={40} height={40} src="img/logo.png" alt="logo" />
           <div>
@@ -22,12 +22,12 @@ function Header(props) {
             <span>{totalPrice} руб.</span>
           </li>
           <li className="mr-20 cu-p">
-            <Link to="/favorites">
+            <Link to="favorites">
               <img width={18} height={18} src="img/heart.svg" alt="bookmarks" />
             </Link>
           </li>
           <li>
-            <Link to="/orders">
+            <Link to="orders">
               <img width={18} height={18} src="img/user.svg" alt="orders" />
             </Link>
           </li>
