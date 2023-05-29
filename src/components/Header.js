995b8afc6e37738698c957c1bdba693a6dd68480
Dzmitry Arhei <demetris167@gmail.com>
2023-05-29 03:12:08 +0300

    lesson 7.1

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ function Header(props) {
     <header className="d-flex justify-between align-center p-40">
       <Link to="/">
         <div className="d-flex align-center">
-          <img width={40} height={40} src="/img/logo.png" alt="logo" />
+          <img width={40} height={40} src="img/logo.png" alt="logo" />
           <div>
             <h3 className="text-uppercase">React Sneakers</h3>
             <p className="opacity-5">Магазин лучших кроссовок</p>
@@ -18,17 +18,17 @@ function Header(props) {
       <div>
         <ul className="d-flex ">
           <li className="mr-30 cu-p" onClick={props.onClickCart}>
-            <img width={18} height={18} src="/img/cart.svg" alt="cart" />
+            <img width={18} height={18} src="img/cart.svg" alt="cart" />
             <span>{totalPrice} руб.</span>
           </li>
           <li className="mr-20 cu-p">
             <Link to="favorites">
-              <img width={18} height={18} src="/img/heart.svg" alt="bookmarks" />
+              <img width={18} height={18} src="img/heart.svg" alt="bookmarks" />
             </Link>
           </li>
           <li>
             <Link to="orders">
-              <img width={18} height={18} src="/img/user.svg" alt="orders" />
+              <img width={18} height={18} src="img/user.svg" alt="orders" />
             </Link>
           </li>
         </ul>
